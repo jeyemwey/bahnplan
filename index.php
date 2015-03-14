9519697892579d02b6d0e778accf98b3879d8c82
Jannik <jeyemwey@live.de>
2015-03-14 23:25:24 +0100

    Add: Marker to the map
    available through marker()-function.

--- index.php
+++ index.php
@@ -41,9 +41,34 @@
 			</ul>
 		</div>
 		<!--<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2549979.6440863633!2d9.767473844438895!3d51.38073571222702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479a721ec2b1be6b%3A0x75e85d6b8e91e55b!2sDeutschland!5e0!3m2!1sde!2sde!4v1426350191057" frameborder="0" style="border:0"></iframe>-->
-		<div id="map_canvas"></div>
+		<div id="map_canvas">
+			<script type="text/javascript">
+			function initialize() {
+				centerP = new google.maps.LatLng(51.165691, 10.451526);
+				var mapOptions = {
+					zoom: 7,
+					center: centerP,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				}
+				var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+				function addMarker(lat, lng, title) {
+					var marker = new google.maps.Marker({
+						position: new google.maps.LatLng(lat, lng),
+						map: map,
+						title: title,
+    					animation: google.maps.Animation.DROP
+					});
+				}
+				addMarker(52.5093520,13.3757390, "Berlin");
+				addMarker(53.5537365,9.9927808, "Hamburg");
+			}
+
+			window.onload = initialize;
+			</script>
+		</div>
 	</div>
-	<script type="text/javascript" src="app/js/app.js"></script>
+	<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
