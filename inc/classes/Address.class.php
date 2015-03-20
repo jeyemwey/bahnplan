d7e3c31321666aa88902b230097c475659bf060a
Jannik <jeyemwey@live.de>
2015-03-20 17:46:45 +0100

    Add: Save
    incl Asking Google for coords (=> Address::getLatLng).

---
+++ inc/classes/Address.class.php
@@ -0,0 +1,28 @@
+<?php
+
+class Address {
+	public $Address;
+
+	public $Lat;
+	public $Lng;
+
+	public function __construct($Address_ = "") {
+		if(!empty($Address_)) {
+			$this->Address = $Address_;
+
+			$this->getLatLng();
+		}
+	}
+
+	private function getLatLng() {
+		echo $Address = urlencode($this->Address);
+		$call = file_get_contents("http://maps.googleapis.com/maps/api/geocode/json?address=". $Address . "&sensor=false");
+
+		$result = json_decode($call);
+
+		$geometry = $result->results[0]->geometry->location;
+
+		echo $this->Lat = $geometry->lat;
+		echo $this->Lng = $geometry->lng;
+	}
+}
