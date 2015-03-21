dd77a0cb83b49ec7c41444a3a40dac8d579bf622
Jannik <jeyemwey@live.de>
2015-03-21 08:53:32 +0100

    Remapping again.

---
+++ inc/classes/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+include "Address.class.php";
+include "Trip.class.php";
+include "TripInMain.class.php";
+
+//External Libraries
+include "Codebird.class.php";
+include "Parsedown.class.php";
