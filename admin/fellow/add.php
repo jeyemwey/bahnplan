59959867b3910fec68912794e812702e7bd31d7a
Jannik <jeyemwey@live.de>
2015-03-22 13:34:09 +0100

    Clean Up

--- admin/fellow/add.php
+++ admin/fellow/add.php
@@ -6,25 +6,19 @@ $return = [];
 header('Content-Type: application/json');
 
 //Grab Things
-$Trip = (int) p($_POST['trip_id'], 1);
-$Twittername = (string) p($_POST['twittername'], "gltyllthsm");
-
-//Get Avatar
-Codebird::setConsumerKey($page["twitter_app_key"], $page["twitter_app_secret"]); // static, see 'Using multiple Codebird instances'
-$cb = Codebird::getInstance();
-$cb->setToken($page["twitter_my_token"], $page["twitter_my_secret"]);
-
-$page["twitter_app_key"] . "\n" . $page["twitter_app_secret"] . "\n" . $page["twitter_my_token"] . "\n" . $page["twitter_my_secret"];
-
-$guy = $cb->users_show("screen_name=" . $Twittername);
-
-$Avatar = $guy->profile_image_url;
-"\n";
-
+$Trip = (int) p($_POST['trip_id']);
+$Twittername = (string) p($_POST['twittername']);
 
 if(empty($Trip) OR empty($Twittername)) {
 	$return["error"] = 406;
 } else {
+	//Get Avatar
+	Codebird::setConsumerKey($page["twitter_app_key"], $page["twitter_app_secret"]);
+	$cb = Codebird::getInstance();
+	$cb->setToken($page["twitter_my_token"], $page["twitter_my_secret"]);
+	$Avatar = $cb->users_show("screen_name=" . $Twittername)->profile_image_url;
+	
+	//Write to DB
 	$sql = "INSERT INTO fellows
 			(id, trip_id, twittername, avatar_url)
 		VALUES (
