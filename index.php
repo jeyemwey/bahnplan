444dc9e9deaf1161be31ea50776354f8e2413a81
Jannik <jeyemwey@live.de>
2015-03-24 14:55:29 +0100

    Move JS to external file and load Bootstrap locally

--- index.php
+++ index.php
@@ -47,56 +47,13 @@ include "inc/init.php";
 			</ul>
 			<footer><?= $Parsedown->text(p($page["footer"])); ?></footer>
 		</div>
-		<div id="map_canvas">
-			<script type="text/javascript">
-			function initialize() {
-				centerP = new google.maps.LatLng(51.165691, 10.451526);
-				var mapOptions = {
-					zoom: 7,
-					center: centerP,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				}
-				var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-
-				//New Markers
-				function addMarker(lat, lng, title) {
-					var marker = new google.maps.Marker({
-						position: new google.maps.LatLng(lat, lng),
-						map: map,
-						title: title,
-    					animation: google.maps.Animation.DROP
-					});
-				}
-				<?php foreach ($Markers as $id => $Marker) : ?>
-					<?php list($Title, $Coords) = $Marker; ?>
-					addMarker(<?= $Coords ?>, "<?= $Title ?>");
-				<?php endforeach; ?>
-
-
-				//Onclick
-				$(".collapse").on("shown.bs.collapse", function() {
-					var lat = $(this).data("lat");
-					var lng = $(this).data("lng");
-
-					console.log(lat + lng);
-
-					$(".collapse:not(#" + $(this).attr("id") + ")").collapse("hide");
-
-
-					//set Map Center
-					map.setZoom(10);
-  					map.setCenter(new google.maps.LatLng(lat, lng));
-				});
-
-			}
-
-			window.onload = initialize;
-			</script>
-		</div>
+		<div id="map_canvas"></div>
 	</div>
+	<script type="text/javascript" src="app/js/application.js"></script>
 	<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js"></script>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="app/js/jquery.min.js"></script>
+    <script type="text/javascript" src="app/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="app/js/bootstrap.collapse.js"></script>
 </body>
 </html>
 <?php include "inc/die.php"; ?>
